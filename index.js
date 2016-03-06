5960e86eb8029f651079274e5d340da1a5a6ad2c
Ephi Gabay <admin@aida-app.com>
2016-03-06 13:56:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var cookie = require('cookie'),
+    co = require('co');
+
+module.exports = function(store, passport) {
+    return function(socket, next) {
+        let cookies = cookie.parse(socket.handshake.headers.cookie);
+        co(function *() {
+            var session = yield store.get('koa:sess:' + cookies['koa.sid']);
+
+            passport.deserializeUser(session.passport.user, (error, user) => {
+
+                if (!error) {
+                    socket.user = user;
+                    return next();
+                }
+
+                next('Authentication error.');
+        });
+
+        })
+    }
+};
