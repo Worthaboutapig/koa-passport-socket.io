38f47f6ecb2a3d18cc4cbe96f9a5a2c5e13c1e82
Jeremy Clarke <jeremy@mellowfruit.com>
2020-01-24 15:07:40 +0000

    Add missing }

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ export default function({ store, cookieName, userDeserializer, logger } = {}) {
       }
 
       if (typeof _cookie !== "string") {
-        logger && logger.error("Cookie is not a string", { data: { cookie: _cookie, handshakeHeaders: socket.handshake.headers, requestHeaders: socket.request.headers.cookie });
+        logger && logger.error("Cookie is not a string", { data: { cookie: _cookie, handshakeHeaders: socket.handshake.headers, requestHeaders: socket.request.headers.cookie } });
       }
       const cookies = cookie.parse(_cookie);
       const sessionId = typeof cookieName === "function" ? cookieName(cookies, socket) : cookieName && cookies[cookieName];
