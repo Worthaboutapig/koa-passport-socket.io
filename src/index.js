bb5c1c26b21a423402ae96692631dd4c0e549a54
Jeremy Clarke <jeremy@savetrees.co.uk>
2020-02-28 11:54:19 +0000

    Reformat layout

--- src/index.js
+++ src/index.js
@@ -3,20 +3,34 @@ import cookie from "cookie";
 export default function({ store, cookieName, userDeserializer, logger } = {}) {
   return async function(socket, next) {
     try {
-      const _cookie = socket.handshake.headers.cookie || socket.request.headers.cookie;
+      const _cookie =
+        socket.handshake.headers.cookie || socket.request.headers.cookie;
       if (!_cookie) {
-        await next("No cookies available on the socket handshake");
+        return await next("No cookies available on the socket handshake");
       }
 
       if (typeof _cookie !== "string") {
-        logger && logger.error("Cookie is not a string", { data: { cookie: _cookie, handshakeHeaders: socket.handshake.headers, requestHeaders: socket.request.headers.cookie } });
+        logger &&
+          logger.error("Cookie is not a string", {
+            data: {
+              cookie: _cookie,
+              handshakeHeaders: socket.handshake.headers,
+              requestHeaders: socket.request.headers.cookie
+            }
+          });
       }
+
       const cookies = cookie.parse(_cookie);
-      const sessionId = typeof cookieName === "function" ? cookieName(cookies, socket) : cookieName && cookies[cookieName];
+      const sessionId =
+        typeof cookieName === "function"
+          ? cookieName(cookies, socket)
+          : cookieName && cookies[cookieName];
       if (sessionId) {
         const session = await store.get(sessionId);
         if (!session) {
-          return next(`Could not locate any user session in the session store using id: ${sessionId}`);
+          return next(
+            `Could not locate any user session in the session store using id: ${sessionId}`
+          );
         }
 
         if (!session.passport || !session.passport.user) {
@@ -26,7 +40,9 @@ export default function({ store, cookieName, userDeserializer, logger } = {}) {
         socket.user = await userDeserializer(session.passport.user);
         await next();
       } else {
-        await next(`Could not locate any session cookie for cookie name: ${cookieName}`);
+        await next(
+          `Could not locate any session cookie for cookie name: ${cookieName}`
+        );
       }
     } catch (error) {
       logger && logger.error(error);
